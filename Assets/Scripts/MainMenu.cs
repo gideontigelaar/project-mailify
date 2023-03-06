9a12bacfb2c33c3c2c15b9f5e059711539b1e563
Gideon <gidtigelaar@gmail.com>
2023-03-06 13:44:05 +0100

    Added navigation menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,41 +3,90 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenu: MonoBehaviour
 {
-    public GameObject LoadingScreen;
-    public GameObject SettingsScreen;
-    public GameObject CreditsScreen;
+    public GameObject Shop;
+    public GameObject Quests;
+    public GameObject Settings;
 
-    public void StartGame()
-    {  
-        SceneManager.LoadScene("Game");
-        LoadingScreen.SetActive(true);
+    public GameObject GameBtn;
+    public GameObject ShopBtn;
+    public GameObject QuestsBtn;
+    public GameObject SettingsBtn;
+
+    public void Start()
+    {
+        GameBtn.GetComponentInChildren<Image>().color = new Color32(0,0,255,255);
+        GameBtn.GetComponentInChildren<TextMeshProUGUI>().color = new Color32(0,0,255,255);
 	}
+
+    public void OpenGame()
+    {
+        Shop.SetActive(false);
+        Quests.SetActive(false);
+        Settings.SetActive(false);
+
+        GameBtn.GetComponentInChildren<Image>().color = new Color32(0,0,255,255);
+        ShopBtn.GetComponentInChildren<Image>().color = new Color32(112,112,112,255);
+        QuestsBtn.GetComponentInChildren<Image>().color = new Color32(112,112,112,255);
+        SettingsBtn.GetComponentInChildren<Image>().color = new Color32(112,112,112,255);
+
+        GameBtn.GetComponentInChildren<TextMeshProUGUI>().color = new Color32(0,0,255,255);
+        ShopBtn.GetComponentInChildren<TextMeshProUGUI>().color = new Color32(112,112,112,255);
+        QuestsBtn.GetComponentInChildren<TextMeshProUGUI>().color = new Color32(112,112,112,255);
+        SettingsBtn.GetComponentInChildren<TextMeshProUGUI>().color = new Color32(112,112,112,255);
+    }
+
+    public void OpenShop()
+    {
+        Shop.SetActive(true);
+        Quests.SetActive(false);
+        Settings.SetActive(false);
+
+        GameBtn.GetComponentInChildren<Image>().color = new Color32(112,112,112,255);
+        ShopBtn.GetComponentInChildren<Image>().color = new Color32(0,0,255,255);
+        QuestsBtn.GetComponentInChildren<Image>().color = new Color32(112,112,112,255);
+        SettingsBtn.GetComponentInChildren<Image>().color = new Color32(112,112,112,255);
+
+        GameBtn.GetComponentInChildren<TextMeshProUGUI>().color = new Color32(112,112,112,255);
+        ShopBtn.GetComponentInChildren<TextMeshProUGUI>().color = new Color32(0,0,255,255);
+        QuestsBtn.GetComponentInChildren<TextMeshProUGUI>().color = new Color32(112,112,112,255);
+        SettingsBtn.GetComponentInChildren<TextMeshProUGUI>().color = new Color32(112,112,112,255);
+    }
+
+    public void OpenQuests()
+    {
+        Shop.SetActive(false);
+        Quests.SetActive(true);
+        Settings.SetActive(false);
+
+        GameBtn.GetComponentInChildren<Image>().color = new Color32(112,112,112,255);
+        ShopBtn.GetComponentInChildren<Image>().color = new Color32(112,112,112,255);
+        QuestsBtn.GetComponentInChildren<Image>().color = new Color32(0,0,255,255);
+        SettingsBtn.GetComponentInChildren<Image>().color = new Color32(112,112,112,255);
+
+        GameBtn.GetComponentInChildren<TextMeshProUGUI>().color = new Color32(112,112,112,255);
+        ShopBtn.GetComponentInChildren<TextMeshProUGUI>().color = new Color32(112,112,112,255);
+        QuestsBtn.GetComponentInChildren<TextMeshProUGUI>().color = new Color32(0,0,255,255);
+        SettingsBtn.GetComponentInChildren<TextMeshProUGUI>().color = new Color32(112,112,112,255);
+    }
 
     public void OpenSettings()
     {
-        SettingsScreen.SetActive(true);
-    }
+        Shop.SetActive(false);
+        Quests.SetActive(false);
+        Settings.SetActive(true);
+        
+        GameBtn.GetComponentInChildren<Image>().color = new Color32(112,112,112,255);
+        ShopBtn.GetComponentInChildren<Image>().color = new Color32(112,112,112,255);
+        QuestsBtn.GetComponentInChildren<Image>().color = new Color32(112,112,112,255);
+        SettingsBtn.GetComponentInChildren<Image>().color = new Color32(0,0,255,255);
 
-    public void CloseSettings()
-    {
-        SettingsScreen.SetActive(false);
-    }
-
-    public void OpenCredits()
-    {
-        CreditsScreen.SetActive(true);
-    }
-
-    public void CloseCredits()
-    {
-        CreditsScreen.SetActive(false);
-    }
-
-    public void QuitGame()
-    {
-        Application.Quit();
+        GameBtn.GetComponentInChildren<TextMeshProUGUI>().color = new Color32(112,112,112,255);
+        ShopBtn.GetComponentInChildren<TextMeshProUGUI>().color = new Color32(112,112,112,255);
+        QuestsBtn.GetComponentInChildren<TextMeshProUGUI>().color = new Color32(112,112,112,255);
+        SettingsBtn.GetComponentInChildren<TextMeshProUGUI>().color = new Color32(0,0,255,255);
     }
 }
