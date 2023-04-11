a05fb27ca12166288a2481af8cf915a24b7f0b48
lucauil <119048381+lucauil@users.noreply.github.com>
2023-04-11 13:21:42 +0200

    customization options work giving food is work in process

--- Assets/Luca/Scripts/GameManager.cs
+++ Assets/Luca/Scripts/GameManager.cs
@@ -17,15 +17,30 @@ public class GameManager : MonoBehaviour
     public GameObject robotPanel;
     public GameObject []robotList;
 
+    public GameObject homePanel;
+    public Sprite[] homeTilesSprites;
+    public GameObject[] homeTiles;
+
+
     public GameObject background;
     public Sprite[] backgroundOptions;
+
+    public GameObject foodPanel;
+    public Sprite[] foodIcons;
 
     private void Start()
     {
         if (!PlayerPrefs.HasKey("looks"))
             PlayerPrefs.SetInt("looks", 0);
         createRobot(PlayerPrefs.GetInt("looks"));
-        createRobot (0);
+
+        if (!PlayerPrefs.HasKey("background"))
+            PlayerPrefs.SetInt("background", 0);
+        changeBackground(PlayerPrefs.GetInt("background"));
+
+        if (!PlayerPrefs.HasKey("looks"))
+            PlayerPrefs.SetInt("looks", 0);
+        createRobot(PlayerPrefs.GetInt("looks"));
     }
     void Update()
     {
@@ -56,10 +71,10 @@ public class GameManager : MonoBehaviour
                 robotPanel.SetActive(!robotPanel.activeInHierarchy);
                 break;
                 case (1):
-
+                homePanel.SetActive(!homePanel.activeInHierarchy);
                 break;
                 case (2):
-
+                foodPanel.SetActive(!foodPanel.activeInHierarchy);
                 break; 
                 case (3):
                 
@@ -76,13 +91,40 @@ public class GameManager : MonoBehaviour
         Destroy(robot);
         robot = Instantiate(robotList[i], Vector3.zero, Quaternion.identity) as GameObject;
 
-        if(robotPanel.activeInHierarchy)
-            robotPanel.SetActive(false);
-        
+        toggle(robotPanel);
+
         PlayerPrefs.SetInt("looks", i);
     }
-    public void OnBeforeTransformParentChanged(int i)
+    public void ChangeTiles(int t)
     {
+        for (int i = 0; i < homeTiles.Length; i++)
+            homeTiles[1].GetComponent<SpriteRenderer>().sprite = homeTilesSprites [t];
+
+        toggle(homePanel);
+
+        if (homePanel.activeInHierarchy)
+            homePanel.SetActive(false);
+    }
+
+    public void changeBackground(int i)
+    {
+        background.GetComponent<SpriteRenderer>().sprite = backgroundOptions[i];
         
+        if (homePanel.activeInHierarchy)
+            homePanel.SetActive(false);
+
+        toggle(homePanel);
+
+        PlayerPrefs.SetInt("background", i);
+    }
+
+    public void selectFood(int i)
+    {
+        toggle(foodPanel);
+    }
+    public void toggle(GameObject g)
+    {
+        if (g.activeInHierarchy)
+            g.SetActive(false);
     }
 }
